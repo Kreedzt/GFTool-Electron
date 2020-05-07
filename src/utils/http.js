c3d987e06ade588696730d8bf61b4dd64947769d
Kreedzt <zhaozisong1@live.com>
2020-05-07 18:44:27 +0800

    :tada: initial commit

---
+++ src/utils/http.js
@@ -0,0 +1,9 @@
+// TODO: axios with GitHub API
+// const axios = require('axios');
+// const { win } = require('..');
+//
+// const http = axios.create();
+//
+// http.interceptors.response.use((res) => {
+//
+// });
